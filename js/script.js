02cdc142c615679f65127fc0f8f7b441f83e97fc
mechanic <furkalokbu@gmail.com>
2021-04-29 13:32:37 +0300

    Class_List

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded',() => {
-    const box = document.querySelector('.box');
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        console.log('start');
-        console.log(e.touches);
-    });
+// console.log(btns[0].classList.length);
+// console.log(btns[1].classList.add('red'));
+// console.log(btns[0].classList.remove('blue'));
 
-    // box.addEventListener('touchmove', (e) => {
-    //     e.preventDefault();
-    //     console.log('move');
-    // });
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
 
-    // box.addEventListener('touchend', (e) => {
-    //     e.preventDefault();
-    //     console.log('end');
-    // });
-
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+    btns[1].classList.toggle('red');
 });
+
+wrapper.addEventListener('click', (event) => {
+    // console.dir(event.target);
+    if (event.target && event.target.matches('button.red')) {
+        console.log("Hello");
+    }
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+//     })
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
