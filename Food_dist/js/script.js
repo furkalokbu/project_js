8f3243f274397ad6aee245e6dbf16e2966522196
Serhii <furkalokbu@gmail.com>
2021-05-30 13:55:30 +0300

    food new files

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -84,4 +84,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadline);
 
+    // Modal
+
+    const modalTrigger = document.querySelector('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.addEventListener('click', () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+    });
+
+    modalCloseBtn.addEventListener('click', () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+    });
+
 });
+
